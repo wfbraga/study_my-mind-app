f772934207b03dc8b0336adccc371a6c3bfe84f1
Wagner Braga <wfbraga@hotmail.com>
2021-02-26 14:31:30 -0300

    ëstudando rotas

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'home#index'
+  
+  get 'home/index' #get 'home', to: 'home#index' tem exatamente o mesmo efeito
 end
