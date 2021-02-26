c7b71b61e5fb46af13ad2f16a514a2e5143ffda7
Wagner Braga <wfbraga@hotmail.com>
2021-02-26 15:42:25 -0300

    add content model

---
+++ db/migrate/20210226182239_create_contents.rb
@@ -0,0 +1,11 @@
+class CreateContents < ActiveRecord::Migration[6.1]
+  def change
+    create_table :contents do |t|
+      t.string :title
+      t.text :description
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
