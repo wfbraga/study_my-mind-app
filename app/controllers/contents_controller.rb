1ef5f21dc131097c5e8a6c6201b6f802dda36427
Wagner Braga <wfbraga@hotmail.com>
2021-03-01 15:49:06 -0300

    Adicionando deleção dos conteudos e fazendo refatoraçao do controler com before_action :set_content

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,5 +1,6 @@
 class ContentsController < ApplicationController
     #skip_berore_action :authenticate_user!
+    before_action :set_content, only: [:show, :edit, :update, :destroy]
     def index
       @contents = current_user.contents
     end
@@ -24,15 +25,12 @@ class ContentsController < ApplicationController
     end
 
     def edit
-      @content = Content.find(params[:id])
     end
     
     def show
-      @content = Content.find(params[:id])
     end
     
     def update
-      @content = Content.find(params[:id])
       
       if @content.update(content_params)
         redirect_to content_path, notice: 'Conteudo Atualizado con suceço'
@@ -42,12 +40,15 @@ class ContentsController < ApplicationController
     end
 
     def destroy
-      
+      @content.destroy
+      redirect_to contents_path, notice: 'Conteudo deletado con suceço'
     end
 
-
-
     private
+
+    def set_content
+      @content = Content.find(params[:id])
+    end
 
     def content_params
       params.require(:content).permit(:title, :description)
